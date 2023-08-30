6204a2ae2d1e28f10a1db6bac0e9f087bfb21e67
Maykel <jotasenator@gmail.com>
2023-08-30 15:07:44 -0400

    Configure middleware for fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,21 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 import { increment, decrement } from "./redux/features/counterSlice";
 
+import {
+  useGetUserByIdQuery,
+  useGetUsersQuery,
+} from "./redux/services/userApi";
+
 const HomePage = () => {
   const count = useAppSelector((state) => state.counterReducer.counter);
 
   const dispatch = useAppDispatch();
+
+  const { data, error, isLoading, isFetching } = useGetUsersQuery(null);
+
+  if (isLoading || isFetching) return <p>Loading...</p>;
+
+  if (error) return <p>There is an error fetching the data.</p>;
 
   const handleIncrement = () => {
     dispatch(increment());
@@ -22,6 +33,14 @@ const HomePage = () => {
       <button onClick={handleIncrement}>increment</button>
       <br />
       <button onClick={handleDecrement}>decrement</button>
+
+      {data?.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.username}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "./features/counterSlice";
+import { userApi } from './services/userApi';
 
 export const store = configureStore({
     reducer: {
-        counterReducer
-    }
+        counterReducer,
+        [userApi.reducerPath]: userApi.reducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat([userApi.middleware])
 })
 
 export type RootState = ReturnType<typeof store.getState>
