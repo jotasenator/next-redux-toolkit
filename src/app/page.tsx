a81c0a33c01dafa4b235f8ee03fa4661fe88447e
Maykel <jotasenator@gmail.com>
2023-08-30 14:36:08 -0400

    Implement increment and decrement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
 import React from "react";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { increment, decrement } from "./redux/features/counterSlice";
 
-const HomePAge = () => {
+const HomePage = () => {
+  const count = useAppSelector((state) => state.counterReducer.counter);
+
+  const dispatch = useAppDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div>
-      <h1>100</h1>
+      <h1>Total:{count}</h1>
 
-      <button>increment</button>
+      <button onClick={handleIncrement}>increment</button>
       <br />
-      <button>dicrement</button>
+      <button onClick={handleDecrement}>decrement</button>
     </div>
   );
 };
 
-export default HomePAge;
+export default HomePage;
